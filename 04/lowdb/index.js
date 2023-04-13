9307adee16359ba83d9845f7d891fbed9a46303f
Noah Jutz <noah@noahjutz.com>
2023-04-13 18:58:01 +0200

    lowdb: update index.js

--- 04/lowdb/index.js
+++ 04/lowdb/index.js
@@ -1,8 +1,5 @@
 import { fileURLToPath } from "node:url";
 import path from "node:path";
-
 import { Low } from "lowdb";
 
-console.log(import.meta.url);
-console.log(fileURLToPath(import.meta.url));
-console.log(path.dirname(fileURLToPath(import.meta.url)));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
