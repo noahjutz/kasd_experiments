fcf9de4133d1324fdfa68df243a54f520b1f9186
Noah Jutz <noahjutz@tutanota.de>
2023-04-14 15:21:03 +0200

    Add lowdb_stack

---
+++ 04/lowdb_stack/README.md
@@ -0,0 +1,41 @@
+# Lowdb with the KASD Stack
+
+## Stack
+
+|           |                         |
+| --------- | ----------------------- |
+| Framework | Hapi + Plugins          |
+| Frontend  | Handlebars + Bulma      |
+| Backend   | Lowdb                   |
+| Style     | ESLint + Prettier       |
+| Test      | Mocha TDD + Chai Assert |
+
+## Installation
+
+```sh
+npm i @hapi/hapi @hapi/vision handlebars lowdb
+npm pkg set type=module # ES Module
+```
+
+## Setup
+
+### ESLint + Prettier
+
+See [01/eslint_prettier](../../01/eslint_prettier/README.md)
+
+### Hapi
+
+See [01/hapi](../../01/hapi/README.md) (TODO make better)
+
+### Vision + Handlebars
+
+See [02/vision](../../02/vision/README.md)
+
+### Mocha + Chai (todo)
+
+See [04/mocha_chai](../../04/mocha_chai/README.md)
+
+### Lowdb
+
+See [04/lowdb#Setup](../../04/lowdb/README.md#setup)
+
---
+++ 04/lowdb_stack/index.js
@@ -0,0 +1,45 @@
+import Hapi from "@hapi/hapi";
+import Vision from "@hapi/vision";
+import { fileURLToPath } from "node:url";
+import { join, dirname } from "node:path";
+import Handlebars from "handlebars";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const db = new Low(new JSONFile(join(__dirname, "db.json")));
+
+const initDb = async () => {
+  await db.read();
+  db.data ||= { users: [] };
+  await db.write();
+};
+
+const initSrv = async () => {
+  const server = Hapi.server({ port: 3000 });
+
+  await server.register(Vision);
+
+  server.views({
+    engines: {
+      hbs: Handlebars,
+    },
+    relativeTo: __dirname,
+    path: "views",
+    partialsPath: "views",
+  });
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      db.read();
+      return h.view("index", { users: db.data.users });
+    },
+  });
+
+  await server.start();
+};
+
+await initDb();
+await initSrv();
