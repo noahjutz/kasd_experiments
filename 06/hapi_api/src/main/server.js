f3c8f5370a2c833c5c8b845dae6749dc2b27c95f
Noah Jutz <noahjutz@tutanota.de>
2023-04-27 16:01:55 +0200

    Add barebones hapi server in server.js

---
+++ 06/hapi_api/src/main/server.js
@@ -0,0 +1,21 @@
+import Hapi from "@hapi/hapi";
+
+const server = Hapi.server({
+  port: 3000,
+});
+
+export const start = async () => {
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: () => "Hello!",
+  });
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+export const stop = async () => {
+  await server.stop();
+  console.log("Server stopped");
+};
--- 06/hapi_api/src/test/test.js
+++ 06/hapi_api/src/test/test.js
@@ -1,7 +1,22 @@
 import { assert } from "chai";
+import axios from "axios";
+import * as server from "../main/server.js";
 
-suite("test", () => {
-  test("test", () => {
-    assert.typeOf("a", "string");
+const client = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
+suite("API", () => {
+  setup(async () => {
+    await server.start();
+  });
+
+  teardown(async () => {
+    await server.stop();
+  });
+
+  test("Entry point returns Hello World", async () => {
+    const response = await client.get("/");
+    console.log(response.data);
   });
 });
