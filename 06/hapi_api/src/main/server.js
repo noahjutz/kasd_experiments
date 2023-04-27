12804fbf40c1c4af83668fd410291c54672d46ea
Noah Jutz <noahjutz@tutanota.de>
2023-04-27 16:08:52 +0200

    hapi_api: Install mongoose, connect to mongodb

--- 06/hapi_api/src/main/server.js
+++ 06/hapi_api/src/main/server.js
@@ -1,4 +1,5 @@
 import Hapi from "@hapi/hapi";
+import mongoose from "mongoose";
 
 const server = Hapi.server({
   port: 3000,
@@ -11,11 +12,13 @@ export const start = async () => {
     handler: () => "Hello, World!",
   });
 
+  await mongoose.connect("mongodb://127.0.0.1:27017/posts");
   await server.start();
   console.log("Server running on %s", server.info.uri);
 };
 
 export const stop = async () => {
   await server.stop();
+  await mongoose.disconnect();
   console.log("Server stopped");
 };
