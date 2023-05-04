4261327ba32f61d0a2675f5f02fd3928f66936b9
Noah Jutz <noahjutz@tutanota.de>
2023-05-04 16:01:22 +0200

    Consolidate app.js and server.js

--- 06/hapi_api/src/main/app.js
+++ 06/hapi_api/src/main/app.js
@@ -1,8 +1,30 @@
-import * as server from "./server.js";
+import Hapi from "@hapi/hapi";
+import mongoose from "mongoose";
+import routes from "./routes.js";
 
-await server.start();
+const server = Hapi.server({
+  port: 3000,
+});
+
+server.route(routes);
+
+const start = async () => {
+  await mongoose.connect("mongodb://127.0.0.1:27017/posts");
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
+
+mongoose.connection.once("open", function () {
+  console.log(`Mongoose connected to ${this.name} on ${this.host}`);
+});
 
 process.on("unhandledRejection", (err) => {
   console.log(err);
   process.exit(1);
 });
+
+await start();
